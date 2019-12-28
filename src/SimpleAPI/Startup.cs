00d7c9375cdd4030333dfa6f3a162d5d4e3d9435
Sree <wupendram@gmail.com>
2019-12-28 11:49:28 +0000

    removed https redirection

--- src/SimpleAPI/Startup.cs
+++ src/SimpleAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace SimpleAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
