340593775ef59e238e8c413727dea75a761b389b
Sree <wupendram@gmail.com>
2019-12-28 11:17:59 +0000

    Initial Commit

---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        WeatherForecastController weatherForecastController = new WeatherForecastController();
+        [Fact]
+        public void GetReturnsCorrectNumber()
+        {
+            var returnValue = weatherForecastController.GetValue();
+            Assert.Equal("Sree", returnValue);
+        }
+
+    }
+}
